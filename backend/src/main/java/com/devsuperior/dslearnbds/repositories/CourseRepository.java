b9996955cc1c63ba17235a9f0e5c5a8aa89b50a5
matheusktt <matheus_0119@outlook.com>
2023-04-12 23:46:48 -0300

    repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.entities.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
