b21f6d725204cfb7377263f86efcbbafb43018c5
matheusktt <matheus_0119@outlook.com>
2023-04-19 00:38:14 -0300

    annotation repository

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/ReplyRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/ReplyRepository.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslearnbds.repositories;
 
 import com.devsuperior.dslearnbds.entities.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslearnbds.repositories;
 
 import com.devsuperior.dslearnbds.entities.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
 }
